c6a5c397cf895bbe621b358f14b51c88d48ba3e9
TimerErTim <timerertim@gmail.com>
2026-05-15 15:34:37 +0200

    feat: greatly reduce embedding size for final decision

--- scripts/graph_deeplearning/src/lib.rs
+++ scripts/graph_deeplearning/src/lib.rs
@@ -7,15 +7,15 @@ pub mod nn;
 pub fn get_model_config() -> PlagiarismDeciderConfig {
     PlagiarismDeciderConfig::new(
         u16::MAX as usize + 1,
-        32,
+        16,
         128,
         0.1,
         vec![
-            PlagiarismDeciderLayerConfig::new(64, 1024).with_pre_aggregations(0),
-            PlagiarismDeciderLayerConfig::new(128, 256).with_pre_aggregations(0),
-            PlagiarismDeciderLayerConfig::new(256, 64).with_pre_aggregations(0),
-            PlagiarismDeciderLayerConfig::new(512, 16).with_pre_aggregations(0),
-            PlagiarismDeciderLayerConfig::new(2048, 1).with_pre_aggregations(0),
+            PlagiarismDeciderLayerConfig::new(32, 1024).with_pre_aggregations(0),
+            PlagiarismDeciderLayerConfig::new(64, 256).with_pre_aggregations(0),
+            PlagiarismDeciderLayerConfig::new(128, 64).with_pre_aggregations(0),
+            PlagiarismDeciderLayerConfig::new(256, 16).with_pre_aggregations(0),
+            PlagiarismDeciderLayerConfig::new(1024, 1).with_pre_aggregations(0),
         ],
     )
 }
